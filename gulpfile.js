89825920782cdb3b278882e29f6c69d8d883ee48
wan999 <steviecavanagh@yahoo.com>
2026-05-26 07:51:05 +0100

    Change thumbnail ratio

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ import imagemin, {mozjpeg, optipng} from 'gulp-imagemin';
             .pipe(gulp.dest('dist'))
   }
 
-  function opitimiseimages() { 
+  function optimiseimages() { 
     return gulp.src('src/img/*', { encoding: false })
         .pipe(imagemin([
           mozjpeg({quality: 75, progressive: true}),
@@ -20,7 +20,7 @@ import imagemin, {mozjpeg, optipng} from 'gulp-imagemin';
         .pipe(gulp.dest('dist/img'))
   }
 
-export default series(copyjs, copymain, opitimiseimages);
+export default series(copyjs, copymain, optimiseimages);
 
 
 
